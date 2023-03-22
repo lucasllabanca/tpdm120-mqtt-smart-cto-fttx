179aef90bc8f50095791940792da0402b927b87b
Lucas Labanca <lucasllabanca@hotmail.com>
2023-03-21 23:44:12 -0300

    feat: criando estrutura publish subscribe mqtt

--- app/app.js
+++ app/app.js
@@ -1,35 +1,43 @@
 var mqtt = require('mqtt');
 
-const servidorMqtt = "demo.thingsboard.io";
-const token = "tXZOmZfuot3NZzF1Keln";
+const servidorMqtt = "test.mosquitto.org";
+const clienteCentralControle = 'SmartCTOFTTx-Client';
 
-console.log('Conectando dispositivo ao servidor MQTT');
-var clienteMqtt  = mqtt.connect('mqtt://'+ servidorMqtt, { username: token });
+var topicos = ["smart/cto/fttx/#/clients/data", "smart/cto/fttx/#/temperature", "smart/cto/fttx/#/clients/umidade"];
+
+console.log('Iniciando aplicação MQTT');
+
+InserirSeparadorLogs();
+
+console.log(`Conectando dispositivo ao servidor MQTT: ${servidorMqtt}`);
+var clienteMqtt = mqtt.connect(`mqtt://${servidorMqtt}`, { clientId: clienteCentralControle });
+
+InserirSeparadorLogs();
 
 clienteMqtt.on('connect', function () {
-    console.log('Cliente conectado ao servidor MQTT!');
-
-    //publicar dados dos clientes a cada 10 segundos
-    setInterval(publicarDadosClientes, 10000);
+    console.log(`Cliente conectado ao servidor MQTT: ${servidorMqtt}`);
 });
 
-function publicarDadosClientes() {
-    var dadosClientes = obterDadosClientes();
-    clienteMqtt.publish('smart/cto/fttx/v1/telemetria/clientes', JSON.stringify(dadosClientes));
-}
+clienteMqtt.on('error', function(error) {
+    console.log(`Não foi possível conectar ao servidor MQTT: ${servidorMqtt}. Erro: ${error}`);
+    process.exit(1);
+});
 
-function obterDadosClientes() {
-    
-    const dados = [
-        {
-            codigo: 1,
-            cliente: "João da Silva",
-            plano: "450MB",
-            download: 425.52,
-            upload: 175.25,
-            status: "Conectado"
-        }
-    ];
+InserirSeparadorLogs();
 
-    return dados;
+console.log(`Inscrevendo cliente '${clienteCentralControle}' da central de controle no tópicos:`);
+topicos.forEach((topico, indice) => console.log(`    ${indice} - Tópico: ${topico}`));
+clienteMqtt.subscribe(topicos);
+
+InserirSeparadorLogs();
+
+clienteMqtt.on('message', function(topic, message) {
+    console.log("message is: " + message);
+    console.log("topic is: " + topic);
+});
+
+function InserirSeparadorLogs() {
+    console.log();
+    console.log('------------------------------------------------------------------------------------------------------');
+    console.log();
 }
